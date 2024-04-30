431cabe26c7a1eca28f2845ac440c7edec42cc91
Arash <arash8877@gmail.com>
2024-04-30 16:56:23 +0200

    created auth context api

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,26 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { login } from "../util/auth";
+import { Alert } from "react-native";
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function loginHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+      await login(email, password);
+    } catch (error) {
+      Alert.alert("authentication failed!" ,"Check your entered email and password.");
+    }
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Login..." />;
+  }
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
--- util/auth.js
+++ util/auth.js
@@ -1,11 +1,32 @@
 import axios from "axios";
-const baseUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
+const signupUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
+const signInUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
 const apiKey = "AIzaSyAa6KIva2yDn6_0ckG3q_B9rBt9m22fYDs";
 
+
+// async function authenticate (mode, email, password) {
+//     const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${apiKey}`;
+//     const response = await axios.post(url, {
+//         email: email,
+//         password: password,
+//         returnSecureToken: true,
+//       });
+// }
+
 export const createUser = async (email, password) => {
-  const response = await axios.post(`${baseUrl}${apiKey}`, {
+  const response = await axios.post(`${signupUrl}${apiKey}`, {
     email: email,
     password: password,
     returnSecureToken: true,
   });
+  console.log(response.data)
 };
+
+export const login = async (email, password) => {
+    const response = await axios.post(`${signInUrl}${apiKey}`, {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    });
+    console.log(response.data)
+  };
