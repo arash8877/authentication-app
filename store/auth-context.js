7b146c20be14c5fe18d6c4999d9d092784115a0d
Arash <arash8877@gmail.com>
2024-05-02 19:55:28 +0200

    store token in the user device to keep user loged in

--- store/auth-context.js
+++ store/auth-context.js
@@ -1,4 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+// AsyncStorage is a package helps storing data in user device.
 
 export const AuthContext = createContext({
   token: "",
@@ -10,12 +12,24 @@ export const AuthContext = createContext({
 function AuthContextProvider({ children }) {
   const [authToken, setAuthToken] = useState();
 
+  useEffect(() => {
+    async function fetchToken() {
+      const storedToken = await AsyncStorage.getItem("token");
+      if (storedToken) {
+        setAuthToken(storedToken);
+      }
+    }
+    fetchToken();
+  }, []);
+
   function authenticate(token) {
     setAuthToken(token);
+    AsyncStorage.setItem("token", token);
   }
 
   function logout() {
     setAuthToken(null);
+    AsyncStorage.removeItem("token");
   }
 
   const value = {
