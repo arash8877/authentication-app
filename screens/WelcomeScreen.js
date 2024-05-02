8a75c629642aac8723469dd6191ebc8952b481ce
Arash <arash8877@gmail.com>
2024-05-02 19:28:16 +0200

    fetching protected data that required authentication

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
+const messageUrl =
+  "https://react-native-expense-app-afc58-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(messageUrl + token);
+        setFetchedMessage(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
@@ -14,13 +36,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
